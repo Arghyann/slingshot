ac6ccd726530d9d9d4508fcd19aab7a21ea62267
Kashew Knutt <kashewknutt@gmail.com>
2024-05-22 01:14:15 +0530

    main

--- main.py
+++ main.py
@@ -12,3 +12,11 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    
+    screen.fill((255,255,255))
+
+    pygame.draw.rect(screen, (0,255,0), (0, 400, 800, 200))
+
+    pygame.draw.rect(screen, (120,0,150), (100,340, 20, 60))
+
+    pygame.display.flip()
