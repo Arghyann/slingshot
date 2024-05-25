57f27a1c6d760d675b8a0a05902e28c045edd533
Arghyann <144401510+Arghyann@users.noreply.github.com>
2024-05-25 21:55:23 +0530

    ground collison

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 import numpy as np
 pygame.init()
 time=pygame.time.Clock()
-cair=1.5                
+cair=2              
 mass=40
 c=cair/mass
 g=-9.8/mass  #because of the stupid coordinate system in pygame (why would you let origin be the top left corner)
@@ -15,19 +15,19 @@ angle=0
 collision=False
 collision_time=None
 t=0
-time_factor=5 #to speed up the simulation
+time_factor=20 #to speed up the simulation
 def velocity_finder(xi):
     return elastic_constant*xi       #how much you pull it back->x
 def xcord(u,s,t):                                   #u and s are the speeds and the angles
-    if collision==False:
-        return originalcords[0]+(u * (np.cos(s)) / c) * (1 - np.exp(-c * t))
-    return collisionCords[0]+(u * (np.cos(s)) / c) * (1 - np.exp(-c * t))
+    
+    return originalcords[0]+(u * (np.cos(s)) / c) * (1 - np.exp(-c * t))
+    
 def ycord(u,s,t):
     term1 = u * (np.sin(s)) + g / c
     term2 = (u * (np.sin(s)) + g / c) * np.exp(-c * t)
-    if collision==False:
-        return originalcords[1]-((term1 - term2) / c) - g * t / c
-    return collisionCords[1]-((term1 - term2) / c) - g * t / c
+    
+    return originalcords[1]-((term1 - term2) / c) - g * t / c
+    
 screen = pygame.display.set_mode((800,600))
 
 #bird co-ordinates
@@ -63,37 +63,41 @@ while run:
         x=np.linalg.norm(currentcords-originalcords)         #displays x
         angle = -(np.arctan2(originalcords[1] - currentcords[1], originalcords[0] - currentcords[0]))       #edit it so that it outputs negative 
         print("New Cords distance: ",x)
-    if currentcords[0] + birdRadius <= 800 and BirdFlying:
+    if currentcords[0] + birdRadius <= 800 and BirdFlying:       
+        t = ((pygame.time.get_ticks() / 1000) - startTime) * time_factor
+        currentcords[0] = xcord(u, angle, t)
+        currentcords[1] = ycord(u, angle, t)
+            
         
-        if collision == False:
-            t = ((pygame.time.get_ticks() / 1000) - startTime) * time_factor
-            currentcords[0] = xcord(u, angle, t)
-            currentcords[1] = ycord(u, angle, t)
-            
-        else:
-            t = ((pygame.time.get_ticks() / 1000) - collision_time) * time_factor
-            currentcords[0] = xcord(u, angle, t) 
-            currentcords[1] = ycord(u, angle, t) 
             
             
-    if(currentcords[1]+birdRadius>=400):                      #when it reaches the ground
-        BirdFlying=False
-    if currentcords[0] + birdRadius >= 800 and BirdFlying:        # When it flies out of the frame
+    
+        
+    if (currentcords[0] + birdRadius >= 800  ) and BirdFlying:        # When it flies out of the frame
         print("updated u and angle after collision")
         print("current cords here", currentcords)
           # Elasticity is 1
-        previous_time = t - 10
+        previous_time = t - 0.05
         
         prevx, prevy = xcord(u, angle, previous_time), ycord(u, angle, previous_time)
         angle = -np.arctan2(-(currentcords[1] - prevy), currentcords[0] - prevx)
-        u=-(np.sqrt((currentcords[0] - prevx)**2 + (currentcords[1] - prevy)**2)/10)
-
+        u=-(np.sqrt((currentcords[0] - prevx)**2 + (currentcords[1] - prevy)**2)/0.05)
+        originalcords=currentcords.copy()
         
-        collision=True
-        collisionCords=np.array([currentcords[0],currentcords[1]])
-        collision_time=pygame.time.get_ticks() /1000
-        print(u)
-    #background 
+        
+       
+        startTime=pygame.time.get_ticks() /1000
+    if currentcords[1]+birdRadius>=400 and BirdFlying:
+        print("updated u and angle after collision")
+        print("current cords here", currentcords)
+          # Elasticity is 1
+        previous_time = t - 0.05
+        
+        prevx, prevy = xcord(u, angle, previous_time), ycord(u, angle, previous_time)
+        angle = (np.pi/2-np.arctan2(currentcords[0]-prevx,currentcords[1]-prevy))
+        u=(np.sqrt((currentcords[0] - prevx)**2 + (currentcords[1] - prevy)**2)/0.05)
+        originalcords=currentcords.copy()
+        startTime=pygame.time.get_ticks() /1000
     screen.fill((255,255,255))
 
     #grass
