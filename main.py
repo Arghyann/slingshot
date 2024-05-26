c15544d585b0f3e22835f99f7126c5fd9e80658e
Arghyann <144401510+Arghyann@users.noreply.github.com>
2024-05-26 21:39:40 +0530

    projection added

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ t=0
 time_factor=50 #to speed up the simulation
 def velocity_finder(xi):
     return elastic_constant*xi       #how much you pull it back->x
-def xcord(u,s,t):                                   #u and s are the speeds and the angles
+def xcord(u,s,z):                                   #u and s are the speeds and the angles
     
-    return originalcords[0]+(u * (np.cos(s)) / c) * (1 - np.exp(-c * t))
+    return originalcords[0]+(u * (np.cos(s)) / c) * (1 - np.exp(-c * z))
     
-def ycord(u,s,t):
+def ycord(u,s,z):
     term1 = u * (np.sin(s)) + g / c
-    term2 = (u * (np.sin(s)) + g / c) * np.exp(-c * t)
+    term2 = (u * (np.sin(s)) + g / c) * np.exp(-c * z)
     
     return originalcords[1]-((term1 - term2) / c) - g * t / c
 def vx(u,s,t):
@@ -34,7 +34,19 @@ def vy(u,s,t):
     term=u*c+g
     return (term*np.exp(-c*t) - g)*c
 screen = pygame.display.set_mode((800,600))
-
+def draw_projection():
+        
+    points = []
+    drawtime=pygame.time.get_ticks() / 1000
+    for i in range(1,500):
+                z = ((pygame.time.get_ticks() / 1000)-drawtime)*time_factor*i  # Incremental time for projection
+                print(u,angle)
+                x = xcord(u, angle, z)
+                y = ycord(u, angle, z)
+                
+                points.append((int(x), int(y)))
+    if len(points) > 1:
+                pygame.draw.lines(screen, (0, 0, 0), False, points, 1)
 #bird co-ordinates
 originalcords=np.array([111,323])
 currentcords=np.array([111,323])
@@ -61,14 +73,19 @@ while run:
             print("Mouse Released")
             BirdFlying=True
             startTime=pygame.time.get_ticks() / 1000
-            u=velocity_finder(x)
+            
             print("u", u,"angle", np.degrees(angle))
 
     if birdHeld:
         currentcords[0], currentcords[1] = pygame.mouse.get_pos()
         x=np.linalg.norm(currentcords-originalcords)         #displays x
         angle = -(np.arctan2(originalcords[1] - currentcords[1], originalcords[0] - currentcords[0]))       #edit it so that it outputs negative 
+        u=velocity_finder(x)
+        startTime=pygame.time.get_ticks() / 1000
+        t=((pygame.time.get_ticks() / 1000) - startTime) * time_factor
         print("bird held:", birdHeld)
+        
+        
     if currentcords[0] + birdRadius <= 800 and currentcords[1]+birdRadius<=400 and BirdFlying:       
         t = ((pygame.time.get_ticks() / 1000) - startTime) * time_factor
         currentcords[0] = xcord(u, angle, t)
@@ -98,7 +115,7 @@ while run:
         BirdFlying=False
     
     screen.fill((255,255,255))
-
+    
     #grass
     pygame.draw.rect(screen, (0,255,0), (0, 400, 800, 200))
 
@@ -111,5 +128,5 @@ while run:
     
     pygame.draw.circle(screen, (255, 0,0), (currentcords[0], currentcords[1]), birdRadius)
 
-
+    if birdHeld: draw_projection()
     pygame.display.flip()
